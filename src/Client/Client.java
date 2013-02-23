7c0b157e665eefe8882f839bcb60ab73951dd1d3
Johan Baversjo <baversjo@gmail.com>
2013-02-23 09:52:03 -0800

    copy paste

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,5 +1,71 @@
 package Client;
 
-public class Client {
+import java.io.*;
 
+import javax.net.ssl.*;
+import java.security.KeyStore;
+
+public class Client {
+	public static void main(String[] args) throws IOException{
+		String host = "localhost";
+		int port = 60212;
+		String path = "";
+		
+		SSLSocketFactory factory = null;
+		
+		try {
+            SSLContext ctx;
+            KeyManagerFactory kmf;
+            KeyStore ks;
+            char[] passphrase = "passphrase".toCharArray();
+
+            ctx = SSLContext.getInstance("TLS");
+            kmf = KeyManagerFactory.getInstance("SunX509");
+            ks = KeyStore.getInstance("JKS");
+
+            ks.load(new FileInputStream("testkeys"), passphrase);
+
+            kmf.init(ks, passphrase);
+            ctx.init(kmf.getKeyManagers(), null, null);
+
+            factory = ctx.getSocketFactory();
+        } catch (Exception e) {
+            throw new IOException(e.getMessage());
+        }
+        
+		SSLSocket socket = (SSLSocket)factory.createSocket(host, port);
+
+		socket.startHandshake();
+		
+		PrintWriter out = new PrintWriter(
+                new BufferedWriter(
+                new OutputStreamWriter(
+                socket.getOutputStream())));
+		
+		out.println("GET " + path + " HTTP/1.0");
+		out.println();
+		out.flush();
+		
+		/*
+         * Make sure there were no surprises
+         */
+        if (out.checkError())
+            System.out.println(
+                "SSLSocketClient: java.io.PrintWriter error");
+
+        /* read response */
+        BufferedReader in = new BufferedReader(
+                                new InputStreamReader(
+                                socket.getInputStream()));
+
+        String inputLine;
+
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+
+        in.close();
+        out.close();
+        socket.close();
+		
+	}
 }
