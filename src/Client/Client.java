698aed5f2b7581a3fdbf02ec6d7b1e5795b31f09
Sven <sven.elfgren@gmail.com>
2013-02-28 00:44:22 +0100

    Added scanning for user input, code doesn't run past handshake. Added serverside arraylists for doctors,nurses,patients,records. Added method structure on server for record handling.

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -4,16 +4,23 @@ import java.io.*;
 
 import javax.net.ssl.*;
 
+import java.util.*;
+
 import java.security.KeyStore;
+
 
 public class Client {
 	public static void main(String[] args) throws IOException{
 		String host = "localhost";
 		int port = 1337;
 		String path = "";
+		Scanner scan = new Scanner(System.in);
 		
 		System.setProperty("javax.net.ssl.trustStore", "keys/hca_trusted.jks");
-		System.setProperty("javax.net.ssl.trustStorePassword", "qweqwe");
+		System.out.println("Enter password:");
+//		String pass = new String(readPassword()); // read pw without echo! replaces scanner, must be run from console.
+ 		String pass = scan.next();
+		System.setProperty("javax.net.ssl.trustStorePassword", pass);
 		
 		SSLSocketFactory factory = null;
 		
@@ -21,7 +28,7 @@ public class Client {
             SSLContext ctx;
             KeyManagerFactory kmf;
             KeyStore ks;
-            char[] passphrase = "qweqwe".toCharArray();
+            char[] passphrase = pass.toCharArray();
 
             ctx = SSLContext.getInstance("TLS");
             kmf = KeyManagerFactory.getInstance("SunX509");
@@ -36,13 +43,54 @@ public class Client {
         } catch (Exception e) {
             throw new IOException(e.getMessage());
         }
+		System.out.println("bajs1");
+
         
 		SSLSocket socket = (SSLSocket)factory.createSocket(host, port);
 		socket.setNeedClientAuth(true);
 		
 		printSocketInfo(socket);
+		System.out.println("bajs2");
 
 		socket.startHandshake();
+		System.out.println("bajs3");
+
+		String list = "list";
+		String create = "create";
+		String delete = "delete";
+		boolean input = true;
+		while(input){
+			System.out.println("Inläsning börjar!");
+			String command = scan.next();
+			if(command.equals(list)){
+				String patient = scan.next();
+				if(patient.equalsIgnoreCase("all")){
+					System.out.println("Listar alla");
+					//TODO metodanrop server!
+				}else{
+					System.out.println("Lista alla med patient " + patient);
+					//TODO metodanrop server!
+				}
+			}else if(command.equals(create)){
+				String patient = scan.next();
+				String doctor = scan.next();
+				String nurse = scan.next();
+				String division = scan.next();
+				String data = scan.next();
+				System.out.println("Skapar journal med: Patient(" + patient + ") " +
+						"Doctor(" + doctor + ") Nurse(" + nurse + ") Division" + division + ") Data(" + data + ")");
+				//TODO metodanrop server! glöm ej kolla access doctor.
+			}else if(command.equals(delete)){
+				String record = scan.next();
+				System.out.println("Tar bort journal: " + record);
+				//TODO metodanrop server glöm ej kolla access admin!
+
+			}else{
+				System.out.println("Inget lästes");
+			}
+			
+			
+		}
 		
 		PrintWriter out = new PrintWriter(
                 new BufferedWriter(
@@ -91,4 +139,8 @@ public class Client {
 	      System.out.println("   Cipher suite = "+ss.getCipherSuite());
 	      System.out.println("   Protocol = "+ss.getProtocol());
   }
+	public static char[] readPassword(){
+		Console cons = System.console();
+		return cons.readPassword();
+	}
 }
